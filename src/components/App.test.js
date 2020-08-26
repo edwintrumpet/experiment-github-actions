e9b9bb185d50d3a0f1fe4a6d781b558fbfc2fe6d
Edwin García <spark.com.co@gmail.com>
2020-08-26 14:31:44 -0500

    Changes in policy

--- src/components/App.test.js
+++ src/components/App.test.js
@@ -10,6 +10,6 @@ describe('<App />', () => {
   });
 
   test('Render del título', () => {
-    expect(app.find('h1').text()).toEqual('Without permission limits!');
+    expect(app.find('h1').text()).toEqual('Changes in policy!');
   });
 });
