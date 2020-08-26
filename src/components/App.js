ecb6bd1d5ab3d641a94d36cb7861b0fd1d82ca5b
edwin <47165886+edwintrumpet@users.noreply.github.com>
2020-08-26 11:33:36 -0500

    Ready to production deploy
    * [feature] enable React
    
    * [feature] continuous delivery
    
    * [feature] continuous deployment
    
    * [bugfix] continuous deployment
    
    * [feature] enable React
    
    * [feature] continuous delivery
    
    * [feature] continuous deployment
    
    * [bugfix] continuous deployment
    
    * [bugfix] continuous deployment
    
    * [test] new ssh (#8)
    
    * Testing (#12)
    
    * [test] new ssh (#8)
    
    * Implementando tests e integracion continua
    
    * Cambios en App
    
    * Ready to production deploy
    
    * [test] new ssh (#8)
    
    * Implementando tests e integracion continua
    
    * Cambios en App
    
    * Ready to deploy

---
+++ src/components/App.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export default function App() {
+  return <h1>Ready to production deploy!</h1>;
+}
---
+++ src/components/App.test.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { mount } from 'enzyme';
+import App from './App';
+
+describe('<App />', () => {
+  const app = mount(<App />);
+
+  test('Render del componente App', () => {
+    expect(app.length).toEqual(1);
+  });
+
+  test('Render del título', () => {
+    expect(app.find('h1').text()).toEqual('Ready to production deploy!');
+  });
+});
